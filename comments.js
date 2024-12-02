c0886059b5d34654d7b614d9fe23d9bf2e28c1de
arrabi <arrabi@gmail.com>
2024-12-02 06:47:08 +0000

    blah

--- comments.js
+++ comments.js
@@ -3,13 +3,3 @@
 // when the route is hit, read the comments.json file
 // and send the contents back to the client
 
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-
-const app = express();
-app.use(express.static('public'));
-
-app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
-});
