8b2ebb2b2204940efad4aac7d9b17d8f6ee71066
arrabi <arrabi@gmail.com>
2024-12-02 06:14:19 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+  var member = {
+    firstName: 'John',
+    lastName: 'Doe',
+    skills: ['JavaScript', 'HTML', 'CSS'],
+    age: 25,
+    isMarried: false
+  }
+  return member;
+}
