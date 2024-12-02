f5697220c62c212867fab0003173a581b0fa25a9
arrabi <arrabi@gmail.com>
2024-12-02 06:45:00 +0000

    fixed

--- comments.js
+++ comments.js
@@ -10,16 +10,6 @@ const path = require('path');
 const app = express();
 app.use(express.static('public'));
 
-app.get('/comments', (req, res) => {
-  fs.readFile(path.join(__dirname, 'comments.json'), 'utf8', (err, data) => {
-    if (err) {
-      res.status(500).send('Error reading comments file');
-    } else {
-      res.send(data);
-    }
-  });
-});
-
 app.listen(3000, () => {
   console.log('Server is listening on port 3000');
 });
